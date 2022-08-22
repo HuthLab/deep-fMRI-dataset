8e33c8c638c9d31a1573cde130306fe542c51d37
Shailee Jain <shailee@cs.utexas.edu>
2022-08-22 16:36:28 -0500

    changed function call to main

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,33 +1,27 @@
+"""
+Get data from openneuro.
+This will make a `data` directory in the main directory of the repo called data if it doesn't already exist.
+"""
 import os
 import pathlib
+import argparse
 
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-download_preprocess", action="store_true",
+						help="download data to disk. If true downloads all preprocessed data.")
+	args = parser.parse_args()
 
-def get_data(download_preprocess=True):
-    """ Get data from openneuro
-    this will make a data directory in the main directory of the repo called data if it doesn't already exist.
+	current_path = pathlib.Path(__file__).parent.resolve()
+	main_dir = pathlib.Path(__file__).parent.parent.resolve()
+	data_dir = os.path.join(main_dir, "data")
 
-    Args:
-        download_preprocess (bool): download data to disk. If true downloads all preprocessed data.
-
-    """
-    
-    current_path = pathlib.Path(__file__).parent.resolve()
-    main_dir = pathlib.Path(__file__).parent.parent.resolve()
-    data_dir = os.path.join(main_dir, 'data')
-
-    os.chdir(main_dir)
-    dir_exist = os.path.isdir(data_dir)
-    if dir_exist:
-        os.chdir(data_dir)
-        os.system('datalad clone git@github.com:OpenNeuroDatasets/ds003020.git')
-        if download_preprocess == True:
-            os.chdir('ds003020')
-            os.system('datalad get derivative/preprocessed_data')
-    else:                      
-        os.system('mkdir data')
-        data_dir = os.path.join(main_dir, 'data')
-        os.chdir(data_dir)
-        os.system('datalad clone git@github.com:OpenNeuroDatasets/ds003020.git')
-        if download_preprocess == True:
-            os.chdir('ds003020')
-            os.system('datalad get derivative/preprocessed_data')
+	os.chdir(main_dir)
+	if not os.path.isdir(data_dir):               
+		os.system("mkdir data")
+		data_dir = os.path.join(main_dir, "data")
+	os.chdir(data_dir)
+	os.system("datalad clone git@github.com:OpenNeuroDatasets/ds003020.git")
+	if args.download_preprocess == True:
+		os.chdir("ds003020")
+		os.system("datalad get derivative/preprocessed_data")
